11bf0cf2c78dcafd7dc709e7f62782c3eea356bb
Mauritz <mauritz.sjodin3@gmail.com>
2023-06-15 06:18:02 +0200

    User can input move again if it's invalid

--- src/main.c
+++ src/main.c
@@ -17,22 +17,30 @@ int main(void) {
             break;
         }
 
-        int start_x, start_y, end_x, end_y;
-        printf("start_x: ");
-        scanf("%d", &start_x);
-        printf("start_y: ");
-        scanf("%d", &start_y);
-        printf("end_x: ");
-        scanf("%d", &end_x);
-        printf("end_y: ");
-        scanf("%d", &end_y);
-
         Move move;
-        move.start_square = &board.squares[start_y][start_x];
-        move.end_square = &board.squares[end_y][end_x];
-        if (is_legal_move(&move, &board)) {
-            make_move(&move, &board);
+        bool legal_move = false;
+        while (!legal_move) {
+            int start_x, start_y, end_x, end_y;
+            printf("start_x: ");
+            scanf("%d", &start_x);
+            printf("start_y: ");
+            scanf("%d", &start_y);
+            printf("end_x: ");
+            scanf("%d", &end_x);
+            printf("end_y: ");
+            scanf("%d", &end_y);
+
+            move.start_square = &board.squares[start_y][start_x];
+            move.end_square = &board.squares[end_y][end_x];
+
+            if (is_legal_move(&move, &board)) {
+                legal_move = true;
+            } else {
+                printf("\nThat's not a legal move\n\n");
+            }
         }
+        make_move(&move, &board);
+
 
         if (player_to_move == WHITE) {
             player_to_move = BLACK;
