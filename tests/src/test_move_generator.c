2b262696338be6cd22fbf90f2210e225fe0fd103
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2023-08-14 21:56:28 +0200

    Made perft function faster

--- tests/src/test_move_generator.c
+++ tests/src/test_move_generator.c
@@ -8,12 +8,13 @@
 
 
 static int perft(int depth, Board* board, MoveArray* move_history) {
-    if (depth == 0) {
-        return 1;
+    MoveArray move_list = get_all_legal_moves(board, move_history);
+
+    if (depth == 1) {
+        return move_list.length;
     }
 
     int nodes = 0;
-    MoveArray move_list = get_all_legal_moves(board, move_history);
     for (int i = 0; i < move_list.length; i++) {
         make_appropriate_move(&move_list.moves[i], board, move_history);
         nodes += perft(depth - 1, board, move_history);    
