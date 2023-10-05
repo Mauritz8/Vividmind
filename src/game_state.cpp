a2a7911a240efd14c22e07817ab94d68eeb96bd3
Mauritz <mauritz.sjodin3@gmail.com>
2023-10-05 11:17:45 +0200

    fix: wrong logic in if condition for legal move validation

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -45,7 +45,7 @@ static bool is_legal(const Move& psuedo_legal, const Board& board, const std::ve
     std::vector<Move> move_history_copy = move_history;
 
     psuedo_legal_copy.make_appropriate(board_copy, move_history_copy);
-    if (is_valid(psuedo_legal) || is_check(board_copy, move_history_copy)) {
+    if (!is_valid(psuedo_legal) || is_check(board_copy, move_history_copy)) {
         return false;
     }
     return true;
