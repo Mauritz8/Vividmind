3aaf1d7e60d662edb42c96f802643ccfc892ea5f
Mauritz <mauritz.sjodin3@gmail.com>
2023-10-28 21:04:53 +0200

    fix: remove code duplication from previous commit

--- include/engine/engine.hpp
+++ include/engine/engine.hpp
@@ -1,6 +1,8 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <climits>
+
 #include "board.hpp"
 #include "board_helper.hpp"
 #include "move.hpp"
@@ -12,8 +14,9 @@ class Engine {
     public:
         Engine(Board& board, BoardHelper& board_helper);
 
-        Move get_best_move(int depth);
         Move iterative_deepening_search(int allocated_time_ms);
+        int search_root(int depth, int time_left);
+        int search_root(int depth);
         void divide(int depth);
 
     private:
@@ -22,12 +25,16 @@ class Engine {
         MoveGenerator move_gen;
         GameOverDetector game_over_detector;
 
+        Move best_move;
         int current_depth;
         int evaluation;
         int nodes_searched;
         int time;
 
-        int search(int depth, int alpha, int beta);
+        static const int NO_TIME_CONSTRAINT = INT_MAX;
+        static const int NO_TIME_LEFT = INT_MIN;
+
+        int search(int depth, int alpha, int beta, int time_left);
         int search_captures(int alpha, int beta);
         int evaluate();
         void show_uci_info() const;
