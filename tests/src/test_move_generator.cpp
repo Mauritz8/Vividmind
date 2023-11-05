5ad823f38a0e07ff27674dce6b61320713321116
Mauritz <mauritz.sjodin3@gmail.com>
2023-11-05 10:14:03 +0100

    add: create my own tests without using google test. You have more control over output, and it's easier.

--- tests/src/test_move_generator.cpp
+++ tests/src/test_move_generator.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,28 +7,30 @@
 #include "move_generator.hpp"
 #include "test_positions.cpp"
 
-class MoveGenerationTest : public testing::Test {
-    public:
-        Board board;
-        MoveGenerator move_gen;
-
-        MoveGenerationTest() 
-            : board(Board())
-            , move_gen(board)
-        {}
+struct Test {
+    std::string fen;
+    int depth;
+    int expected_nodes;
+    int actual_nodes;
 };
 
-TEST_F(MoveGenerationTest, MoveGenerationTestSuite) {
+int main() {
+    Board board;
+    MoveGenerator move_gen(board);
+
+    int total_duration = 0;
+    std::vector<Test> failed_tests;
+    const int nr_tests = test_positions.size();
     for (int i = 0; i < test_positions.size(); i++) {
         std::string position = test_positions.at(i);
-        size_t pos;
+        size_t delim_index;
         std::vector<std::string> tokens;
         while (true) {
-            pos = position.find(';');
-            tokens.push_back(position.substr(0, pos)); 
-            position.erase(0, pos + 1);
+            delim_index = position.find(';');
+            tokens.push_back(position.substr(0, delim_index)); 
+            position.erase(0, delim_index + 1);
             
-            if (pos == std::string::npos) break;
+            if (delim_index == std::string::npos) break;
         }
 
         const std::string fen = tokens.at(0);
@@ -40,20 +42,35 @@ TEST_F(MoveGenerationTest, MoveGenerationTestSuite) {
             const size_t split_index = token.find(' ');
 
             std::string depth_str = token.substr(0, split_index); 
-            const int depth = std::stoi(depth_str.substr(1, std::string::npos));
+            const int depth = std::stoi(depth_str.substr(1));
+            const int expected_nodes = std::stoi(token.substr(split_index)); 
 
-            token.erase(0, split_index + 1);
-            const int expected_nodes = std::stoi(token) + 1; 
-
-
+            const auto start = std::chrono::high_resolution_clock::now();
             const int actual_nodes = move_gen.perft(depth);
-            EXPECT_EQ(actual_nodes, expected_nodes);      
-            if (actual_nodes == expected_nodes) {
-                std::cout << "depth " << depth 
-                << ": found " << actual_nodes 
-                << "/" << expected_nodes << " positions\n"; 
+            const auto finished = std::chrono::high_resolution_clock::now();
+            const int duration = std::chrono::duration_cast<std::chrono::milliseconds>(finished - start).count();
+            total_duration += duration;
+            const bool is_successful = actual_nodes == expected_nodes;
+            std::cout << "depth " << depth << ": found ";
+            std::cout << actual_nodes << "/" << expected_nodes << " moves"; 
+            std::cout << " - " << (is_successful ? "[OK]" : "[FAIL]");
+            std::cout << " (" << "" << duration << " ms)" << "\n";
+
+            if (!is_successful) {
+                failed_tests.push_back(Test{fen, depth, expected_nodes, actual_nodes});
             }
         }
         std::cout << "\n";
+    }
+
+    const int nr_failed_tests = failed_tests.size();
+    std::cout << "\n\nFinished in " << total_duration / 1000 << " s\n";
+    std::cout << nr_tests - nr_failed_tests << "/" << nr_tests << " tests were successful\n\n";
+    if (nr_failed_tests > 0) {
+        std::cout << "Failed tests:\n";
+        for (Test test : failed_tests) {
+            std::cout << "FEN: " << test.fen << "(depth " << test.depth << " found ";
+            std::cout << test.actual_nodes << "/" << test.expected_nodes << " moves)\n"; 
+        }
     }
 }
