5af30c6fd7d9758e848da902c722132b83280ae4
Mauritz <mauritz.sjodin3@gmail.com>
2023-11-07 20:31:02 +0100

    add: store halfmove clock and fullmove number

--- include/board.hpp
+++ include/board.hpp
@@ -18,8 +18,10 @@ struct Castling {
 
 struct GameState {
     Color player_to_move;
-    std::optional<int> en_passant_square;
     std::array<Castling, 2> castling_rights;
+    std::optional<int> en_passant_square;
+    int halfmove_clock;
+    int fullmove_number;
     std::array<int, 2> material;
     std::array<int, 2> psqt;
     std::optional<Piece> captured_piece;
