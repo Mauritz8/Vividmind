40d6ea23175ea3724b72d8925878686f23d38651
Mauritz <mauritz.sjodin3@gmail.com>
2023-11-09 04:48:08 +0100

    refactor: move some functions to a defs.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -203,28 +203,3 @@ void Search::check_termination() {
         case INFINITE: break;
     }
 }
-
-SearchParams::SearchParams() {
-    depth = MAX_PLY;
-    allocated_time = 0;
-    game_time = GameTime{
-        .wtime = 0,
-        .btime = 0,
-        .winc = 0,
-        .binc = 0,
-        .moves_to_go = 0
-    };
-    search_mode = INFINITE;
-}
-
-SearchInfo::SearchInfo() {
-    start_time = std::chrono::high_resolution_clock::now();
-    depth = 0;
-    nodes = 0;
-    is_terminated = false;
-}
-
-int SearchInfo::time_elapsed() const {
-    auto now = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
-}
