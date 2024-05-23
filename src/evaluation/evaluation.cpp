e5fbabb41d4ef02b1ff19ab8b248d62b3e33fc85
Mauritz <mauritz.sjodin3@gmail.com>
2024-05-23 09:52:03 +0200

    refactor: more declarative evaluation function

--- src/evaluation/evaluation.cpp
+++ src/evaluation/evaluation.cpp
@@ -4,31 +4,15 @@
 #include "piece.hpp"
 #include "utils.hpp"
 
-// the evaluation function is very straight forward
-// it only considers material and piece-square tables
 int Board::evaluate() const {
-
-  // the evaluation from white's point of view
-  int evaluation = 0;
-
-  // calculate the material balance from white's point of view
   const int white_material = game_state.material.at(WHITE);
   const int black_material = game_state.material.at(BLACK);
-  evaluation += white_material - black_material;
-
-  // calculate the difference between the piece-square table values
-  // also from white's point of view
   const int white_psqt = game_state.psqt.at(WHITE);
   const int black_psqt = game_state.psqt.at(BLACK);
-  evaluation += white_psqt - black_psqt;
 
-  // always return the score from the view of the player to move
-  // so a positive value always means a good position
-  // and a negative value means a bad position
-  if (game_state.player_to_move == BLACK) {
-    return -evaluation;
-  }
-  return evaluation;
+  const int evaluation =
+      white_material - black_material + white_psqt - black_psqt;
+  return game_state.player_to_move == BLACK ? -evaluation : evaluation;
 }
 
 int Piece::get_value() const {
