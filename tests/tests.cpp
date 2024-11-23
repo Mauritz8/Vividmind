bce25c43a97aaf49507b72a8f162df8afed661a6
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2024-11-23 15:03:30 +0100

    test: create cmake target that runs all tests

---
+++ tests/tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "test_move.cpp"
+#include "test_gen_pseudo_legal_moves.cpp"
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
