112285bbdc3bf19ec7cd3e442b6fc3b99ab2d2a2
Mauritz <mauritz.sjodin3@gmail.com>
2025-04-15 12:47:09 +0200

    feat: use bitwise operations instead of hardware instructions
    that way it works no matter the operating system

--- src/bitboards_board/bits.cpp
+++ src/bitboards_board/bits.cpp
@@ -1,5 +1,4 @@
 #include "bits.hpp"
-#include <byteswap.h>
 #include <optional>
 #include <strings.h>
 
@@ -11,11 +10,28 @@ void set(uint64_t &bits, int n) { bits |= (uint64_t)1 << n; }
 
 void unset(uint64_t &bits, int n) { bits ^= (uint64_t)1 << n; }
 
-std::optional<int> popLSB(u_int64_t &bits) {
-  const int i = ffsl(bits);
-  if (i != 0) {
-    unset(bits, i - 1);
-    return i - 1;
+//https://www.chessprogramming.org/BitScan#DeBruijnMultiplation
+const int index64[64] = {
+  0,  1,  48,  2, 57, 49, 28,  3,
+  61, 58, 50, 42, 38, 29, 17,  4,
+  62, 55, 59, 36, 53, 51, 43, 22,
+  45, 39, 33, 30, 24, 18, 12,  5,
+  63, 47, 56, 27, 60, 41, 37, 16,
+  54, 35, 52, 21, 44, 32, 23, 11,
+  46, 26, 40, 15, 34, 20, 31, 10,
+  25, 14, 19,  9, 13,  8,  7,  6
+};
+int first_set_bit(uint64_t bits) {
+    if (bits == 0) return -1;
+    bits &= -bits;
+    return index64[(bits * 0x03F79D71B4CB0A89) >> 58];
+}
+
+std::optional<int> popLSB(uint64_t &bits) {
+  const int i = first_set_bit(bits);
+  if (i != -1) {
+    unset(bits, i);
+    return i;
   }
   return std::nullopt;
 }
@@ -49,6 +65,20 @@ uint64_t reverse(uint64_t b) {
          (b >> 48);
 }
 
-int nr_bits_set(u_int64_t bits) { return __builtin_popcountl(bits); }
+// https://www.chessprogramming.org/Population_Count#Lookup
+const uint8_t lookup_table[256] = {
+  #define B2(n)  n,      n+1,      n+1,      n+2
+  #define B4(n)  B2(n),  B2(n+1),  B2(n+1),  B2(n+2)
+  #define B6(n)  B4(n),  B4(n+1),  B4(n+1),  B4(n+2)
+  B6(0), B6(1), B6(1), B6(2)
+};
+int nr_bits_set(uint64_t bits) {
+  uint64_t count = 0;
+  for (int i = 0; i < 8; i++) {
+    count += lookup_table[bits & 0xFF];
+    bits >>= 8;
+  }
+  return count;
+}
 
 } // namespace bits
